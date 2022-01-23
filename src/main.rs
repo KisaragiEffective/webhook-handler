093812fdbca891a3557e40d8678c3a81dc364eeb
Kisaragi <kisaragi.effective@gmail.com>
2022-01-23 19:31:19 +0900

    update: #

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 #![warn(clippy::pedantic, clippy::nursery)]
-#![deny(type_alias_bounds)]
+#![deny(type_alias_bounds, legacy_derive_helpers)]
 mod provider;
 mod payload;
 
+use std::any::Any;
+use std::borrow::Borrow;
+use std::collections::HashMap;
+use std::marker::PhantomData;
 use once_cell::sync::OnceCell;
 use std::sync::{Arc, RwLock};
 use actix_web::{App, guard, HttpRequest, HttpResponse, HttpServer, web};
 use qstring::QString;
+use serde::{Deserialize, Deserializer, Serialize};
 use serde_json::{json, Value};
 use provider::all::{Discord, Todoist};
-use crate::provider::all::{DestinationProviderRegistry, SourceProviderRegistry, Registry};
+use crate::payload::todoist::TodoistPayload;
+use crate::provider::all::{SourceProvider, GenRegistry, DestinationProvider, Sender, RecvSend};
 
 // https://stackoverflow.com/questions/54406029/how-can-i-parse-query-strings-in-actix-web
 async fn api_post(request: HttpRequest) -> HttpResponse {
@@ -18,22 +24,23 @@ async fn api_post(request: HttpRequest) -> HttpResponse {
     let to_provider = qs.get("to");
     from_provider.map_or_else(
         || HttpResponse::BadRequest().json(BadRequestErrors::required_query_parameter("from")),
-        |from_provider| {
+        |from_provider_name| {
             to_provider.map_or_else(
                 || HttpResponse::BadRequest().json(BadRequestErrors::required_query_parameter("to")),
-                |to_provider| {
-                    let source_registry = &SOURCE_PROVIDER_REGISTRY.get().unwrap().read().unwrap();
-                    source_registry.get_by_name(from_provider).map_or_else(
+                |to_provider_name| {
+                    let source_registry = SOURCE_PROVIDER_REGISTRY.borrow().get().unwrap().read().unwrap();
+                    source_registry.get_by_key(from_provider_name).map_or_else(
                         || HttpResponse::BadRequest().json(BadRequestErrors::indicated_unsupported_platform(
                             "from",
-                            source_registry.registered_provider_names())),
+                            source_registry.registered_keys())),
                         |from_provider| {
-                            let dest_registry = &DESTINATION_PROVIDER_REGISTRY.get().unwrap().read().unwrap();
-                            dest_registry.get_by_name(to_provider).map_or_else(
+                            let dest_registry = DESTINATION_PROVIDER_REGISTRY.borrow().get().unwrap().read().unwrap();
+                            dest_registry.get_by_key(to_provider_name).map_or_else(
                                 || HttpResponse::BadRequest().json(BadRequestErrors::indicated_unsupported_platform(
                                     "to",
-                                    dest_registry.registered_provider_names())),
+                                    dest_registry.registered_keys())),
                                 |to_provider| {
+
                                     HttpResponse::NoContent().finish()
                                 }
                             )
@@ -55,7 +62,7 @@ impl BadRequestErrors {
         })
     }
 
-    fn indicated_unsupported_platform(name: &str, supported_platforms: Vec<&String>) -> Value {
+    fn indicated_unsupported_platform(name: &str, supported_platforms: Vec<&&str>) -> Value {
         json!({
             "code": 400,
             "reason": "query parameter 'from' indicates unsupported platform",
@@ -65,22 +72,53 @@ impl BadRequestErrors {
     }
 }
 
-static SOURCE_PROVIDER_REGISTRY: OnceCell<Arc<RwLock<SourceProviderRegistry>>> = OnceCell::new();
-static DESTINATION_PROVIDER_REGISTRY: OnceCell<Arc<RwLock<DestinationProviderRegistry>>> = OnceCell::new();
+// bypass E0225
+trait SizedDeserialize<'a> : Send + Sync + Deserialize<'a> {}
+trait SizedSerialize : Send + Sync + Serialize {}
+
+trait TypeTag {
+    type Tag;
+}
+
+struct DiscordTypeTag;
+impl TypeTag for DiscordTypeTag { type Tag = Discord; }
+
+struct TodoistTypeTag;
+impl TypeTag for TodoistTypeTag { type Tag = TodoistPayload; }
+
+struct X<'a, D: SizedDeserialize<'a>, S: SizedSerialize> {
+    de: D,
+    ser: S,
+    __phantom_data: PhantomData<&'a ()>
+}
+
+impl <'a, D: TypeTag + SizedDeserialize<'a>, S: TypeTag + SizedSerialize> Into<RecvSend<'a, Box<D::Tag>, Box<S::Tag>>> for X<'a, D, S> {
+    fn into(self) -> RecvSend<'a, Box<D>, Box<S>> {
+        RecvSend::new()
+    }
+}
+
+type ConcurrentGenRegistry<K, V> = Arc<RwLock<GenRegistry<K, V>>>;
+static RECV_SEND_REGISTRY: OnceCell<ConcurrentGenRegistry<(&'static str, &'static str), RecvSend<'static, Box<dyn SizedDeserialize>, Box<dyn SizedSerialize>>>> = OnceCell::new();
+static SOURCE_PROVIDER_REGISTRY: OnceCell<ConcurrentGenRegistry<&'static str, &Box<&'static dyn SourceProvider<InputType = dyn Any>>>> = OnceCell::new();
+static DESTINATION_PROVIDER_REGISTRY: OnceCell<ConcurrentGenRegistry<&'static str, &Box<&'static dyn DestinationProvider<OutputType = dyn Any>>>> = OnceCell::new();
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    SOURCE_PROVIDER_REGISTRY.set(Arc::new(RwLock::new(SourceProviderRegistry::new()))).ok().unwrap();
-    DESTINATION_PROVIDER_REGISTRY.set(Arc::new(RwLock::new(DestinationProviderRegistry::new()))).ok().unwrap();
-    if let Some(ad) = SOURCE_PROVIDER_REGISTRY.get() {
-        let mut register = ad.write().unwrap();
-        register.register("discord", &(Todoist));
-        // register.register_source_provider("github", GitHub);
+    RECV_SEND_REGISTRY.set(Arc::new(RwLock::new(GenRegistry::new()))).ok().unwrap();
+    if let Some(recv_send) = RECV_SEND_REGISTRY.get() {
+        let mut recv_send = recv_send.write().unwrap();
+        recv_send.register(("todoist", "discord"), X {
+            de: TodoistTypeTag,
+            ser: DiscordTypeTag
+        }.into())
     }
 
     if let Some(ad) = DESTINATION_PROVIDER_REGISTRY.get() {
         let mut register = ad.write().unwrap();
-        register.register("todoist", &(Discord));
+        let y: Box<&dyn Any> = Box::new(&Todoist);
+        let z = y.downcast_ref::<Box<&'static dyn DestinationProvider<OutputType = dyn Any>>>();
+        register.register("discord", z.unwrap());
     }
     HttpServer::new(|| {
         App::new()
@@ -112,3 +150,5 @@ async fn main() -> std::io::Result<()> {
         .run()
         .await
 }
+
+struct TypeBox<T>(T);
--- src/provider/all.rs
+++ src/provider/all.rs
@@ -1,13 +1,80 @@
 use std::any::Any;
 use std::collections::HashMap;
+use std::error::Error;
+use std::future::Future;
+use std::hash::Hash;
+use std::marker::PhantomData;
 use std::ops::Deref;
 use std::process::Output;
 use std::sync::{Arc, Mutex};
+use iso8601::DateTime;
+use reqwest::Response;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use crate::payload::todoist::TodoistPayload;
+use crate::SizedDeserialize;
+use anyhow::{Context, Result};
 
 type Entry<'a, K, V> = (&'a K, &'a V);
 type ArcMut<A> = Arc<Mutex<A>>;
 type DerefTarget<D> = <D as Deref>::Target;
+
+#[derive(Default)]
+pub struct RecvSend<'de, In, Out> {
+    __phantom_de: PhantomData<&'de ()>,
+    __phantom_in: PhantomData<In>,
+    __phantom_out: PhantomData<Out>
+}
+
+impl <'de, In, Out> RecvSend<'de, In, Out> {
+    async fn on_receive<F: FnOnce(In) -> Out>(&self, raw_json: &'de str, to: String, f: F) -> Result<()>
+        where In: Deserialize<'de> + Sized + Send + Sync, Out: Serialize + Sized + Send + Sync {
+        let x = reqwest::Client::new();
+        let y = serde_json::from_str::<In>(raw_json)
+            .map(f)
+            .map(|a| x.post(to).json(&a))
+            .map(|rb| rb.send());
+
+        match y {
+            Ok(a) => {
+                a.await.map(|_| ()).context("oops!")
+            }
+            Err(b) => {
+                Err(b).context("oops!")
+            }
+        }
+    }
+
+    pub(crate) fn new<'d, I, O>() -> RecvSend<'d, I, O> {
+        RecvSend {
+            __phantom_de: PhantomData,
+            __phantom_in: PhantomData,
+            __phantom_out: PhantomData,
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct Sender<Out, Env> {
+    __out_phantom: PhantomData<Out>,
+    __env_phantom: PhantomData<Env>,
+}
+
+impl <Out, Env> Sender<Out, Env> {
+    async fn send(to: String, out: Out) {
+        todo!("fuck")
+    }
+}
+
+pub struct Receiver<In> {
+    __in_phantom: PhantomData<In>
+}
+
+impl <In> Receiver<In> {
+    fn parse() {
+
+    }
+}
 
 pub trait DestinationProvider: Send + Sync {
     type OutputType;
@@ -17,88 +84,73 @@ pub trait SourceProvider: Send + Sync {
     type InputType;
 }
 
-pub(crate) trait Registry : Send + Sync {
-    type Value;
-    fn register(&mut self, name: &str, registering_value: Self::Value);
-    fn get_by_name(&self, name: &str) -> Option<&ArcMut<&'static DerefTarget<Self::Value>>> where Self::Value: Deref;
-    fn entries(&self) -> Vec<Entry<'_, String, ArcMut<&'static DerefTarget<Self::Value>>>> where Self::Value: Deref;
-    fn registered_provider_names(&self) -> Vec<&String>;
+pub struct GenRegistry<K: Hash + Eq + Sync, V> {
+    register: HashMap<K, V>
 }
 
-#[derive(Default)]
-pub struct SourceProviderRegistry {
-    pub(crate) provider: HashMap<String, ArcMut<&'static (dyn SourceProvider<InputType = &'static dyn Any> + Send + Sync)>>
-}
+impl <K: Hash + Eq + Sync, V> GenRegistry<K, V> {
+    pub(crate) fn register(&mut self, k: K, v: V) {
+        self.register.insert(k, v);
+    }
 
-impl SourceProviderRegistry {
-    pub(crate) fn new() -> Self {
-        SourceProviderRegistry {
-            provider: HashMap::new()
+    fn unregister(&mut self, k: K) {
+        &mut self.register.remove(&k);
+    }
+
+    pub(crate) fn get_by_key(&self, k: K) -> Option<&V> {
+        self.register.get(&k)
+    }
+
+    pub fn registered_keys(&self) -> Vec<&K> {
+        self.register.keys().collect()
+    }
+
+    fn entries(&self) -> Vec<(&K, &V)> {
+        self.register.iter().collect()
+    }
+
+    pub(crate) fn new() -> GenRegistry<K, V> {
+        GenRegistry {
+            register: HashMap::new()
         }
     }
 }
 
-impl Registry for SourceProviderRegistry {
-    type Value = &'static (dyn SourceProvider<InputType = &'static dyn Any> + Send + Sync);
-
-    fn register(&mut self, name: &str, registering_value: Self::Value) {
-        self.provider.insert(name.to_string(), Arc::new(Mutex::new(registering_value)));
-    }
-
-    fn get_by_name(&self, name: &str) -> Option<&ArcMut<&'static DerefTarget<Self::Value>>> {
-        self.provider.get(name).to_owned()
-    }
-
-    fn entries(&self) -> Vec<Entry<'_, String, ArcMut<&'static DerefTarget<Self::Value>>>> {
-        self.provider.iter().collect()
-    }
-
-    fn registered_provider_names(&self) -> Vec<&String> {
-        self.provider.keys().collect()
+impl <K: Hash + Eq + Sync, V> GenRegistry<K, V> {
+    pub(crate) fn register_into<L>(&mut self, k: K, v: L) where L: Into<V> {
+        &mut self.register.insert(k, v.into());
     }
 }
 
-#[derive(Default)]
-pub struct DestinationProviderRegistry {
-    pub(crate) provider: HashMap<String, ArcMut<&'static (dyn DestinationProvider<OutputType = &'static dyn Any> + Send + Sync)>>
+trait E0207Bypass {
+    type X;
 }
 
-impl DestinationProviderRegistry {
-    pub(crate) fn new() -> Self {
-        DestinationProviderRegistry {
-            provider: HashMap::new()
-        }
-    }
+impl <T> E0207Bypass for T { type X = T; }
+
+pub struct MiddlewareRegistry {
+
 }
 
-impl Registry for DestinationProviderRegistry {
-    type Value = &'static (dyn DestinationProvider<OutputType = &'static dyn Any> + Send + Sync);
+pub struct Middleware<T, U> {
+    input_provider_name: String,
+    output_provider_name: String,
+    phantom_t: PhantomData<T>,
+    phantom_u: PhantomData<U>,
+}
 
-    fn register(&mut self, name: &str, registering_value: Self::Value) {
-        self.provider.insert(name.to_string(), Arc::new(Mutex::new(registering_value)));
-    }
+impl <T: SourceProvider, U: DestinationProvider> Middleware<T, U> {
 
-    fn get_by_name(&self, name: &str) -> Option<&ArcMut<&'static DerefTarget<Self::Value>>> {
-        self.provider.get(name)
-    }
-
-    fn entries(&self) -> Vec<Entry<'_, String, ArcMut<&'static <Self::Value as Deref>::Target>>> {
-        self.provider.iter().collect()
-    }
-
-    fn registered_provider_names(&self) -> Vec<&String> {
-        self.provider.keys().collect()
-    }
 }
 
 pub struct Discord;
-
-pub struct Todoist;
 
 impl DestinationProvider for Discord {
     type OutputType = &'static dyn Any;
 }
 
+pub struct Todoist;
+
 impl SourceProvider for Todoist {
-    type InputType = &'static dyn Any;
+    type InputType = &'static TodoistPayload;
 }
