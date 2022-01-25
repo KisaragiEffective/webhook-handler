d8830adabe53bae96361cb0f6923551c459a7c4c
Kisaragi <kisaragi.effective@gmail.com>
2022-01-26 00:22:49 +0900

    update: support serialize and deserialize for ApiKey

--- src/call/api_key.rs
+++ src/call/api_key.rs
@@ -1,5 +1,7 @@
 use rand::prelude::*;
 use rand_chacha::ChaCha20Rng;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde::de::Error;
 
 pub struct ApiKey(pub(in crate::call) [u8; 64]);
 
@@ -11,5 +13,43 @@ impl ApiKey {
         let mut data = [0u8; 64];
         csp_rng.fill_bytes(&mut data);
         ApiKey(data)
+    }
+
+    fn as_base64(&self) -> String {
+        base64::encode(self)
+    }
+}
+
+impl <'de> Deserialize<'de> for ApiKey {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+        String::deserialize(deserializer).and_then(|a| base64::decode(a).map_err(|f| D::Error::custom(f))).map(|a| ApiKey::from(a))
+    }
+}
+
+impl Serialize for ApiKey {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+        base64::encode(self).serialize(serializer)
+    }
+}
+
+impl AsRef<[u8]> for ApiKey {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl From<Vec<u8>> for ApiKey {
+    fn from(a: Vec<u8>) -> Self {
+        if a.len() != 64 {
+            panic!("ApiKey bytes != 64")
+        } else {
+            let mut x = [0u8; 64];
+            let mut p = 0usize;
+            for m in a {
+                x[p] = m;
+                p += 1;
+            }
+            ApiKey(x)
+        }
     }
 }
